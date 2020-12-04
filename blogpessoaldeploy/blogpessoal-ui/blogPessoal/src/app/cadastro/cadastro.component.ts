9895a57a3bc7dd37128c0f7bfc37194d30c56118
Eduardo Freitas <edu.sf98@hotmail.com>
2020-12-04 18:41:00 -0300

    Implementação dos componentes de cadastro e login

--- blogpessoaldeploy/blogpessoal-ui/blogPessoal/src/app/cadastro/cadastro.component.ts
+++ blogpessoaldeploy/blogpessoal-ui/blogPessoal/src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent implements OnInit {
-  senha: string
-  constructor() { }
 
-  ngOnInit(){
+  user: User = new User()
+  senha: string
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+  
+  cadastrar() {
+    if ( this.senha === this.user.senha ) {
+      this.authService.cadastrar(this.user).subscribe((resp: any = User) => {
+        this.user = resp
+        this.router.navigate(['/login'])
+        alert('Usuário cadastrado com sucesso!')
+      })
+    } else {
+      alert('Suas senhas não conferem')
+    }
+    
   }
 
   conferirSenha(event: any) {
--- blogpessoaldeploy/blogpessoal-ui/blogPessoal/src/app/perfil-lateral/perfil-lateral.component.spec.ts
+++ blogpessoaldeploy/blogpessoal-ui/blogPessoal/src/app/perfil-lateral/perfil-lateral.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+
 import { PerfilLateralComponent } from './perfil-lateral.component';
 
 describe('PerfilLateralComponent', () => {
